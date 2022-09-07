997bb1e0dbfd49edeb98133df492aecc3de520a1
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-09-07 06:48:24 +0200

    Add search box, background image and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+const api = {
+  key: '9e04c0b1257be867050e0db17ee68aea',
+  base: 'https://api.openweathermap.org/data/2.5/'
+}
+
+// console.log(api.key)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className='search-bar'
+            placeholder="Search..."
+          />
+        </div>
+      </main>
     </div>
   );
 }
