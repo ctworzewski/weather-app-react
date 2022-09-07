3f0d6d307582a682fa5571bf902117b25c7c0227
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-09-07 07:16:44 +0200

    Return in my browser  function dateBuilder

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 const api = {
-  key: '9e04c0b1257be867050e0db17ee68aea',
-  base: 'https://api.openweathermap.org/data/2.5/'
-}
+  key: "9e04c0b1257be867050e0db17ee68aea",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 // console.log(api.key)
 function App() {
-
   const dateBuilder = (d) => {
-    let months = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
-    let days  = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
+    let months = [
+      "Styczeń",
+      "Luty",
+      "Marzec",
+      "Kwiecień",
+      "Maj",
+      "Czerwiec",
+      "Lipiec",
+      "Sierpień",
+      "Wrzesień",
+      "Październik",
+      "Listopad",
+      "Grudzień",
+    ];
+    let days = [
+      "Niedziela",
+      "Poniedziałek",
+      "Wtorek",
+      "Środa",
+      "Czwartek",
+      "Piątek",
+      "Sobota",
+    ];
 
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-    
-    console.log(day, month, date, year)
-  }
+
+    console.log(day, month, date, year);
+
+    return `${day}, ${date} ${month} ${year}`;
+  };
   return (
     <div className="app">
       <main>
         <div className="search-box">
-          <input 
-            type="text"
-            className='search-bar'
-            placeholder="Search..."
-          />
+          <input type="text" className="search-bar" placeholder="Search..." />
         </div>
         <div className="location-box">
           <div className="location">Kielce, PL</div>
